afb5ec35fbc783b21a5434ebf4bec1eb32c2af6b
Raphael_Linux <raphael.campospt@gmail.com>
2024-02-29 19:16:15 +0000

    backup

--- srcs/stack_utils.c
+++ srcs/stack_utils.c
@@ -1,35 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stack_sorted.c                                     :+:      :+:    :+:   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rapcampo <rapcampo@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 10:23:59 by rapcampo          #+#    #+#             */
-/*   Updated: 2024/02/28 10:24:42 by rapcampo         ###   ########.fr       */
+/*   Updated: 2024/02/29 14:21:40 by rapcampo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-bool	stack_sorted(t_stack *stack)
+t_stack	*find_last_node(t_stack *head)
 {
-	if (!stack)
-		return (1);
-	while (stack->next)
-	{
-		if (stack->nbr > stack->next->nbr)
-			return (false);
-		stack = stack->next;
-	}
-	return (true);
+	if (head == NULL)
+		return (NULL);
+	while (head->next)
+		head = head->next;
+	return (head);
 }
 
-t_stack	*find_min(t_stack *stack)
+void	append_node(t_stack **stack, int nbr)
 {
-	long	min;
-	t_stack	*min_node;
+	t_stack	*node;
+	t_stack	*last_node;
 
-	if (!stack)
-		return (NULL);
+	if (stack == NULL)
+		return ;
+	node = malloc(sizeof(t_stack));
+	if (node == NULL)
+		return ;
+	node->next = NULL;
+	node->nbr = nbr;
+	if (*stack == NULL)
+	{
+		*stack = node;
+		node->prev = NULL;
+	}
+	else
+	{
+		last_node = find_last_node(*stack);
+		last_node->next = node;
+		node->prev = last_node;
+	}
 }
