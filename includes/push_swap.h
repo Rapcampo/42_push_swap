19f26b4de2f07fe3e85c43ff406a093d1cb3e58d
Raphael Campos Vieira <rapcampo@c2r1s10.42porto.com>
2024-08-15 17:57:12 +0100

    fixed

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -17,8 +17,14 @@
 //# include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-# define INT_MAX 2147483647 
-# define INT_MIN -2147483648 
+
+# ifndef INT_MAX
+#  define INT_MAX 2147483647
+# endif
+
+# ifndef INT_MIN
+#  define INT_MIN -2147483648 
+# endif
 
 typedef struct s_elem
 {
